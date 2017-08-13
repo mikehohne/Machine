f5732faba540aa4bc34bfd50aa7e8327cbc1219e
Mike Hohne <mikehohne@gmail.com>
2017-08-13 11:27:38 -0600

    fold structure is setup for compiling code to either prod or dev

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log(process.env.API_KEY);
+import test from './test';
+import * as firebase from "firebase";
+
+test('Production is working!!!')
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTHDOMAIN + ".firebaseapp.com",
+    databaseURL: process.env.DBNAME + ".firebaseio.com",
+    storageBucket: process.env.STORAGEBUCKET + ".appspot.com",
+  };
+  firebase.initializeApp(config);
+
+//   firebase.auth()
+//   .createUserWithEmailAndPassword('andrewhohne@gmail.com', 'testing1234')
+//   .catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//  });
